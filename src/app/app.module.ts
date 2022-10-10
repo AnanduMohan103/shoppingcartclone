48e2d3e5c4458c7a03e4ad9bdae79f4eb4ae3ab4
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-10 21:33:04 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ShoppingcartComponent
   ],
   imports: [
     BrowserModule,
